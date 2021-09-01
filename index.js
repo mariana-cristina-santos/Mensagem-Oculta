96c2dba02ff46ba5dac2f2bf7e8f9a833cf4e303
Mariana Cristina <marianacristinasantos05@gmail.com>
2021-09-01 01:18:14 -0300

    fiz alteraçoes no css e javascript

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var selecionar = document.querySelector("select");
 selecionar.addEventListener("change", function (evento) {
     evento.preventDefault();
 
-    var chave = document.getElementById("incremento");
+    var chave = document.getElementById("numero");
 
     if (evento.target.value == "caesar") {
         chave.style = "display: block";
@@ -33,3 +33,64 @@ btns.forEach((radio) => {
 });
 
 //*** */
+var formulario = document.forms.formulario;
+
+formulario.addEventListener('submit', function (evento) {
+    evento.preventDefault();
+
+    var mensagem = formulario.mensagem.value;
+    var selecionar = formulario.selecionar.value;
+    var incremento = formulario.incremento.value;
+    var radios = formulario.botoes.value;
+    var resultado = '';
+
+    if(selecionar == 'caesar'){
+        resultado = caesar(radios, mensagem, incremento);
+    }
+
+    else{
+        resultado = base64(radios, mensagem);
+    }
+
+    var resultadoMensagem = document.getElementById('resultado');
+    resultadoMensagem.innerHTML = `<p>A mensagem codificada é:</p> ${resultado}`;
+
+    
+
+    formulario.mensagem.focus();
+});
+
+function base64(botoes, mensagem){
+    if(botoes == 'codificar'){
+        return btoa(mensagem);
+    }
+
+    else{
+        return atob(mensagem);
+    }
+}
+
+function caesar(botoes, mensagem, incremento){
+    incremento = Number(incremento);
+
+    var resultado = '';
+
+    for(var i = 0; i < mensagem.length; i++) {
+        var letra = mensagem[i];
+        var codigo = letra.charCodeAt();
+
+        if (botoes == 'codificar') {
+            codigo += incremento;
+        }
+
+        else{
+            codigo -= incremento;
+        }
+
+        resultado += String.fromCharCode(codigo);
+    }
+
+    return resultado;
+}
+
+
